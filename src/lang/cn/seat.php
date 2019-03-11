8184587abedd7336a7b6ce850fe3cc31a3964d4f
Matthew Yu <qilqil@live.com>
2019-03-12 07:03:36 +1100

    trans(chinese): Add Chinese support (#36)

---
+++ src/lang/cn/seat.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of discord-connector and provides user synchronization between both SeAT and a Discord Guild
+ *
+ * Copyright (C) 2016, 2017, 2018  Loïc Leuilliot <loic.leuilliot@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'user_mapping' => '用户对应',
+    'quick_create' => '快速创建',
+    'authorization' => '认证|授权',
+    'user_filter' => '按玩家',
+    'role_filter' => '按角色',
+    'corporation_filter' => '按公司',
+    'title_filter' => '按游戏内头衔',
+    'alliance_filter' => '按联盟',
+    'public_filter' => '所有人',
+    'discord_role' => 'Discord角色',
+    'join'      => '获取discord权限',
+];
