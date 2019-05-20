8993bd0db12ab60d019eac750d01f84851ab4206
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-05-20 23:43:26 +0200

    lang(russian): provide russian support

---
+++ src/lang/ru/seat.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of discord-connector and provides user synchronization between both SeAT and a Discord Guild
+ *
+ * Copyright (C) 2016, 2017, 2018, 2019  Loïc Leuilliot <loic.leuilliot@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return [
+    'user_mapping'       => 'Отображение пользователей',
+    'quick_create'       => 'Быстрое создание',
+    'authorization'      => 'Авторизация',
+    'user_filter'        => 'Пользовательский фильтр',
+    'role_filter'        => 'Фильтр по ролям',
+    'corporation_filter' => 'Корпоративный фильтр',
+    'title_filter'       => 'Фильтр по званиям',
+    'alliance_filter'    => 'Альянсовый фильтр',
+    'public_filter'      => 'Публичный фильтр',
+    'discord_role'       => 'Роль в Discord',
+    'join'               => 'Подключить Discord',
+];
